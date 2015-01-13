a1722454909cb520f2adb2a7d6ea1310d86633a9
Michael Bashour <michael.s.bashour@gmail.com>
2015-01-13 15:31:06 -0600

    Completed prototype, prior to MVP adjustments

--- questionEngine.js
+++ questionEngine.js
@@ -1,34 +1,56 @@
 // This is the Presenter
 
+// (function () {
 
-// Show the qText of the current Question
+// 	window.Quiz = {
+// 		score: 1
+// 	}
+
+// 	Quiz.answerQuestion = function (questionNum, answerIndex) {
+// 		// Maniuplate score
+// 		// True or false
+// 	}
+// })()
 
 var q = 0
+var score = 1
+var quizEngine = function(){
+	hideAlerts()
+	$(".currentQ").text(questions[q]['qText'])
+	$(".ansA").text(questions[q]['qAnswers']['a'])
+	$(".ansB").text(questions[q]['qAnswers']['b'])
+	$(".ansC").text(questions[q]['qAnswers']['c'])
+	$(".ansD").text(questions[q]['qAnswers']['d'])
+	
 
-$(".currentQ").text(questions[q]['qText'])
-$(".ansA").text(questions[q]['qAnswers']['a'])
-$(".ansB").text(questions[q]['qAnswers']['b'])
-$(".ansC").text(questions[q]['qAnswers']['c'])
-$(".ansD").text(questions[q]['qAnswers']['d'])
-
-$(".answer").click(function(){
-	console.log("you clicked", $(this).data('ans') )
-	if($(this).data('ans') === questions[q]['qCorrect']){
-			console.log("correct")
-			hideAll()
-			$(".qCorrect").show()
+	$(".answer").click(function(){
+		if($(this).data('ans') === questions[q]['qCorrect']){
+				hideAlerts()
+				$(".score").text("Score: " + score )
+				$(".qCorrect").show()
+				$(".nextDiv").show()
+			}
+			else {
+				hideAlerts()
+				$(".qIncorrect").show()
+			}
+	})
+}
+	$(".nextQ").click(function(){
+		if(q + 1 < questions.length){
+			score ++ 
+			q ++
+			quizEngine()
+		} else {
+			$(".jumbotron").hide()
 		}
-		else {
-			console.log("incorrect")
-			hideAll()
-			$(".qIncorrect").show()
-		}
+	})
 
 
-})
-
-var hideAll = function(){
+var hideAlerts = function(){
 			$(".instruction").hide()
 			$(".qCorrect").hide()
 			$(".qIncorrect").hide()
+			$(".nextDiv").hide()
 }
+quizEngine()
--- questions.js
+++ questions.js
@@ -6,14 +6,34 @@
 				// qText: "What is 2 * 2?",
 				// qAnswers:{a:1, b:2, c:3, d:4},
 				// qCorrect: "d"}, ...]
+
 var questions = [
 
-	{qNum: 1,
+	{	
+		qNum: 1,
 		qText: "How many states are in the USA?",
 		qAnswers: {	a: 25,
 					b: 40,
 					c: 50,
 					d: 65},
 		qCorrect: "c"
+	},
+	{	
+		qNum: 2,
+		qText: "What is the capitol of Texas?",
+		qAnswers: {	a: "Austin",
+					b: "Dallas",
+					c: "Houston",
+					d: "San Antonio"},
+		qCorrect: "a"
+	},
+	{	
+		qNum: 3,
+		qText: "When was the war of 1812?",
+		qAnswers: {	a: "1936",
+					b: "1812",
+					c: "1776",
+					d: "2032"},
+		qCorrect: "b"
 	}
 ]
