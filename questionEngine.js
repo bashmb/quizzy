9b18969d8539165c8f083d166b3bc32882fa6c61
Michael Bashour <michael.s.bashour@gmail.com>
2015-01-15 11:14:50 -0600

    basic files for mithril

--- questionEngine.js
+++ questionEngine.js
@@ -2,31 +2,45 @@
 
 var q = Quiz['q']
 
+
 var resetQuiz = function() {
+	qData = Quiz.createQuestion()
+
+	console.log(qData['qCorrectPosition'])
 	hideAlerts()
+	$(".flag").attr("src","img/" + qData['qState'].toLowerCase() + ".jpg")
 	$(".answer").removeClass("disabled")
-	$(".currentQ").text(questions[q]['qText'])
-	$(".ansA").text(questions[q]['qAnswers']['a'])
-	$(".ansB").text(questions[q]['qAnswers']['b'])
-	$(".ansC").text(questions[q]['qAnswers']['c'])
-	$(".ansD").text(questions[q]['qAnswers']['d'])
+	$(".answer").removeClass("btn-success")
+	$(".answer").removeClass("btn-danger")				
+	$(".answer").addClass("btn-info")
+	$(".currentQ").text(qData['qState'] + "?")
+	$(".ans" + qData['qCorrectPosition']).text(qData['qCorrectCity'])
+	$(".ans" + qData['qOtherOrder'][0]).text(qData['qOthers'][0])
+	$(".ans" + qData['qOtherOrder'][1]).text(qData['qOthers'][1])
+	$(".ans" + qData['qOtherOrder'][2]).text(qData['qOthers'][2])
 }
 
 var quizEngine = function(){
 
 	$(".answer").click(function(){
-		if(Quiz.answerQuestion($(this).data('ans'), questions[q]['qCorrect'])){
+
+		if(Quiz.answerQuestion($(this).data('ans'), qData['qCorrectPosition'])){
 			hideAlerts()
 			// $(".answer").data('disabled', true);
+			$(".answer").removeClass("btn-info")
+			$(this).addClass("btn-success")
 			$(".answer").addClass('disabled');
-			$(".score").text("Score: " + Quiz['score'] )
-			$(".qCorrect").show()
+			$(".score").text( Quiz['score'] )
+			$(".qCorrect").text(qData['randomCorrect']).show()
 			$(".nextDiv").show()
 		}
 		else {
 			hideAlerts()
+			$(".answer").removeClass("btn-info")
+			$(this).addClass("btn-danger")
+			$(".ans" + qData['qCorrectPosition']).addClass("btn-success")
 			$(".answer").addClass('disabled');
-			$(".qIncorrect").show()
+			$(".qIncorrect").text(qData['randomIncorrect']).show()
 			$(".nextDiv").show()
 		}
 	})
@@ -39,5 +53,30 @@ var hideAlerts = function(){
 			$(".qIncorrect").hide()
 			$(".nextDiv").hide()
 }
+
+var signin = function(){
+	 $("form.sign-in").submit(function(e) {
+    	e.preventDefault();
+    	name = $('[data-id="name"]').val()
+		$(".username").text(name)
+		$(".welcome").hide()
+		$(".highScores").hide()
+		$(".game").show()
+	})
+}
+
+
+
+$("#hiScores").click(function(){
+	$(".welcome").hide()
+	$(".game").hide()
+	$(".highScores").show()
+	Quiz.endOfQuiz()
+	$(".hi1").text(scores).sort()
+
+
+})
+
 resetQuiz()
 quizEngine()
+signin()
--- questions.js
+++ questions.js
@@ -11,20 +11,69 @@
 	Quiz.answerQuestion = function (questionNum, answerIndex) {
 		if(questionNum === answerIndex){
 			Quiz['score'] += 1
+			// Quiz['q'] +=1
+			// var progVal = (Quiz.q + 1)/10
+			// $('.progress').css('width', progVal+'%').attr('aria-valuenow', Quiz.q)
+			// // $(".progress").attr("style","width: "+(Quiz.q/10) + "%")			
 			return true
 		} else {
 			return false
+
 		}
 	}
+
+	Quiz.endOfQuiz = function(){
+		
+		localStorage.setItem(name, Quiz['score'])
+		scores =[]
+		for (var key in localStorage){
+   			scores.push([key, localStorage.getItem(key)])
+		}
+
+
+
+		console.log(localStorage)
+	}	
+
 	Quiz.nextQuestion = function(){
 		$(".nextQ").click(function(){
-			if(q + 1 < questions.length){
+			if(q + 1 < 2){
 				q++
 				resetQuiz()
 			} else {
 				$(".jumbotron").hide()
+				$(".fScore").text(Quiz.score + " out of 10")
+				$(".finalScore").show()
+				Quiz.endOfQuiz()
 			}
 		})
+	}
+
+	Quiz.createQuestion = function(){
+		var random = Math.floor(Math.random()*stateCapitols.length)
+
+		var newQ = {
+			qNum:1,
+			qState: stateCapitols[random][0],
+			qCorrectPosition: Math.floor(Math.random()*3),
+			qCorrectCity: stateCapitols[random][1],
+			qOthers: [stateCapitols[random][2],stateCapitols[random][3],stateCapitols[random][4]],
+			qOtherOrder: [2,3,4],
+			randomCorrect: correctResponses[Math.floor(Math.random()*correctResponses.length)],
+			randomIncorrect: incorrectResponses[Math.floor(Math.random()*incorrectResponses.length)]
+		}
+		 // newQ['qOtherOrder'] = [1,2,3]
+		if(newQ['qCorrectPosition'] === 0){
+				newQ['qOtherOrder'] = [1,2,3]
+			} else if(newQ['qCorrectPosition'] === 1) {
+				newQ['qOtherOrder'] = [0,2,3]
+			} else if(newQ['qCorrectPosition'] === 2){
+				newQ['qOtherOrder'] = [0,1,3]
+			} else if(newQ['qCorrectPosition'] === 3){
+				newQ['qOtherOrder'] = [0,1,2]
+			}
+				
+		return newQ
 	}
 
 })()
@@ -40,87 +89,75 @@
 				// qAnswers:{a:1, b:2, c:3, d:4},
 				// qCorrect: "d"}, ...]
 
-var questions = [
+var correctResponses = [
+	"cool",
+	"nice",
+	"sweet",
+	"word",
+	"woah",
+	"fa sho"
+	]
 
-	{	
-		qNum: 1,
-		qText: "How many states are in the USA?",
-		qAnswers: {	a: 25,
-					b: 40,
-					c: 50,
-					d: 65},
-		qCorrect: "c"
-	},
-	{	
-		qNum: 2,
-		qText: "What is the capitol of Texas?",
-		qAnswers: {	a: "Austin",
-					b: "Dallas",
-					c: "Houston",
-					d: "San Antonio"},
-		qCorrect: "a"
-	},
-	{	
-		qNum: 3,
-		qText: "When was the war of 1812?",
-		qAnswers: {	a: "1936",
-					b: "1812",
-					c: "1776",
-					d: "2032"},
-		qCorrect: "b"
-	}
-]
+var incorrectResponses = [
+	"do you even lift, bro?",
+	"huh",
+	"why'd you get it wrong, bro?",
+	"nah dawg",
+	"yeah.  no.",
+	"i can't even"
+	]
+
 
 var stateCapitols = [
-	{"Alabama":["Montgomery", "Huntsville", "Birmingham", "Mobile"]},
-	{"Alaska":["Juneau", "Fairbanks", "Anchorage", "Sitka"]},
-	{"Arizona":["Phoenix", "Tucson", "Scottsdale", "Sedona"]},
-	{"Arkansas":["Little Rock", "Fayetteville", "Hot Springs", "Fort Smith"]},
-	{"California":["Sacremento", "Los Angeles", "San Francisco", "San Diego"]}
+	["Alabama","Montgomery", "Huntsville", "Birmingham", "Mobile"],
+	["Alaska","Juneau", "Fairbanks", "Anchorage", "Sitka"],
+	["Arizona","Phoenix", "Tucson", "Scottsdale", "Sedona"],
+	["Arkansas","Little Rock", "Fayetteville", "Hot Springs", "Fort Smith"],
+	["California","Sacremento", "Los Angeles", "San Francisco", "San Diego"],
+	["Colorado","Denver", "Boulder", "Aurora", "Aspen"],
+	["Connecticut","Hartford", "New Haven", "Stamford", "Greenwich"],
+	["Delaware","Dover", "Newark", "Lewes","Wilmington"],
+	["Florida","Tallahassee","Miami","Orlando","Tampa"],
+	["Georgia","Atlanta","Savannah", "Augusta","Marietta"],
+	["Hawaii","Honolulu","Kailua","Haleiwa","Kaneohe"],
+	["Idaho","Boise","Idaho Falls","Pocatello","Coeur d'Alene"],
+	["Illinois","Springfield","Chicago","Peoria","Champaign"],
+	["Indiana","Indianapolis","Fort Wayne","Bloomington","South Bend"],
+	["Iowa","Des Moines", "Cedar Rapids","Ames","Dubuque"],
+	["Kansas","Topeka","Wichita","Kansas City","Overland Park"],
+	["Kentucky","Frankfort", "Louisville","Lexington","Elizabethtown"],
+	["Louisiana","Baton Rouge", "New Orleans", "Lafayette", "Shreveport"],
+	["Maine","Augusta", "Portland", "Bangor", "Ogunquit"],
+	["Maryland","Annapolis", "Baltimore", "Frederick", "Ocean City"],
+	["Massachusetts","Boston", "Cambridge", "Salem", "Worcester"],
+	["Michigan","Lansing", "Detroit", "Grand Rapids", "Flint"],
+	["Minnesota","Saint Paul", "Minneapolis", "Duluth", "Rochester"],
+	["Mississippi","Jackson", "Biloxi", "Gulfport", "Hattiesburg"],
+	["Missouri","Jefferson City", "Springfield","St. Louis", "Kansas City"],
+	["Montana","Helena", "Missoula", "Billings", "Bozeman"],
+	["Nebraska","Lincoln", "Omaha", "Kearney", "Grand Island"]
 ]
-	// {"Colorado":["Denver"]},
-	// {"Connecticut":["Hartford"]},
-	// {"Delaware":["Dover"]},
-	// {"Florida":["Tallahassee"]},
-	// {"Georgia":["Atlanta"]},
-	// {"Hawaii":["Honolulu"]},
-	// {"Idaho":["Boise"]},
-	// {"Illinois":["Springfield"]},
-	// {"Indiana":["Indianapolis"]},
-	// {"Iowa":["Des Moines"]},
-	// {"Kansas":["Topeka"]},
-	// {"Kentucky":["Frankfort"]},
-	// {"Louisiana":["Baton Rouge"]},
-	// {"Maine":["Augusta"]},
-	// {"Maryland":["Annapolis"]},
-	// {"Massachusetts":["Boston"]},
-	// {"Michigan":["Lansing"]},
-	// {"Minnesota":["Saint Paul"]},
-	// {"Mississippi":["Jackson"]},
-	// {"Missouri":["Jefferson City"]},
-	// {"Montana":["Helena"]},
-	// {"Nebraska":["Lincoln"]},
-	// {"Nevada":["Carson City"]},
-	// {"New Hampshire":["Concord"]},
-	// {"New Jersey":["Trenton"]},
-	// {"New Mexico":["Santa Fe"]},
-	// {"New York":["Albany"]},
-	// {"North Carolina":["Raleigh"]},
-	// {"North Dakota":["Bismarck"]},
-	// {"Ohio":["Columbus"]},
-	// {"Oklahoma":["Oklahoma City"]},
-	// {"Oregon":["Salem"]},
-	// {"Pennsylvania":["Harrisburg"]},
-	// {"Rhode Island":["Providence"]},
-	// {"South Carolina,":["Columbia"]},
-	// {"South Dakota":["Pierre"]},
-	// {"Tennessee":["Nashville"]},
-	// {"Texas":["Austin"]},
-	// {"Utah":["Salt Lake City"]},
-	// {"Vermont":["Montpelier"]},
-	// {"Virginia":["Richmond"]},
-	// {"Washington":["Olympia"]},
-	// {"West Virginia":["Charleston"]},
-	// {"Wisconsin":["Madison"]},
-	// {"Wyoming":["Cheyenne"]}
+	// ["Nevada","Carson City"],
+	// ["New Hampshire","Concord"],
+	// ["New Jersey","Trenton"],
+	// ["New Mexico","Santa Fe"],
+	// ["New York","Albany"],
+	// ["North Carolina","Raleigh"],
+	// ["North Dakota","Bismarck"],
+	// ["Ohio","Columbus"],
+	// ["Oklahoma","Oklahoma City"],
+	// ["Oregon","Salem"],
+	// ["Pennsylvania","Harrisburg"],
+	// ["Rhode Island","Providence"],
+	// ["South Carolina,","Columbia"],
+	// ["South Dakota","Pierre"],
+	// ["Tennessee","Nashville"],
+	// ["Texas","Austin"],
+	// ["Utah","Salt Lake City"],
+	// ["Vermont","Montpelier"],
+	// ["Virginia","Richmond"],
+	// ["Washington","Olympia"],
+	// ["West Virginia","Charleston"],
+	// ["Wisconsin","Madison"],
+	// ["Wyoming","Cheyenne"]
 	// ]	
